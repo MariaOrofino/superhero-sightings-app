0282e2464b71256097aad0764bd2a27bf956902a
Travis Rogers <7ravis@gmail.com>
2017-07-09 15:22:44 -0500

    added CRUD unit tests for HeroDaoJdbcImpl

--- superhero/src/test/java/twrog/superhero/dao/HeroDaoJdbcImplTest.java
+++ superhero/src/test/java/twrog/superhero/dao/HeroDaoJdbcImplTest.java
@@ -1,18 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package twrog.superhero.dao;
 
-import java.util.List;
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.springframework.jdbc.core.JdbcTemplate;
 import twrog.superhero.dto.Hero;
 
 /**
@@ -23,36 +17,22 @@ public class HeroDaoJdbcImplTest {
     
     public HeroDaoJdbcImplTest() {
     }
-
+    
     /**
-     * Test of addHero method, of class HeroDaoJdbcImpl.
+     * Test of addHero and getHeroByID methods, of class HeroDaoJdbcImpl.
      */
     @Test
-    public void testAddHero() {
+    public void testAddGetHero() {
         System.out.println("addHero");
+        HeroDaoJdbcImpl instance = new HeroDaoJdbcImpl();
         Hero hero = new Hero();
         hero.setHeroName("Wonder Woman");
         hero.setDescription("classic superhero");
-        HeroDaoJdbcImpl instance = new HeroDaoJdbcImpl();
         instance.addHero(hero);
-
+        Hero result = instance.getHeroByID(hero.getHeroID());
+        assertEquals(hero, result);
     }
-
-    /**
-     * Test of getHeroByID method, of class HeroDaoJdbcImpl.
-     */
-    @Test
-    public void testGetHeroByID() {
-        System.out.println("getHeroByID");
-        int heroID = 0;
-        HeroDaoJdbcImpl instance = new HeroDaoJdbcImpl();
-        Hero expResult = null;
-        Hero result = instance.getHeroByID(heroID);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
+    
     /**
      * Test of getAllHeros method, of class HeroDaoJdbcImpl.
      */
@@ -60,123 +40,52 @@ public class HeroDaoJdbcImplTest {
     public void testGetAllHeros() {
         System.out.println("getAllHeros");
         HeroDaoJdbcImpl instance = new HeroDaoJdbcImpl();
-        List<Hero> expResult = null;
-        List<Hero> result = instance.getAllHeros();
+        Hero hero1 = new Hero();
+        hero1.setHeroName("Wonder Woman");
+        hero1.setDescription("classic superhero");
+        instance.addHero(hero1);
+        Hero hero2 = new Hero();
+        hero2.setHeroName("Spider Man");
+        hero2.setDescription("bitten by radio-active spider");
+        instance.addHero(hero2);
+        int expResult = 2;
+        int result = instance.getAllHeros().size();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
-
-    /**
-     * Test of getHerosByLocationID method, of class HeroDaoJdbcImpl.
-     */
-    @Test
-    public void testGetHerosByLocationID() {
-        System.out.println("getHerosByLocationID");
-        int locationID = 0;
-        HeroDaoJdbcImpl instance = new HeroDaoJdbcImpl();
-        List<Hero> expResult = null;
-        List<Hero> result = instance.getHerosByLocationID(locationID);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getHerosByOrganizationID method, of class HeroDaoJdbcImpl.
-     */
-    @Test
-    public void testGetHerosByOrganizationID() {
-        System.out.println("getHerosByOrganizationID");
-        int organizationID = 0;
-        HeroDaoJdbcImpl instance = new HeroDaoJdbcImpl();
-        List<Hero> expResult = null;
-        List<Hero> result = instance.getHerosByOrganizationID(organizationID);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
+    
     /**
      * Test of updateHero method, of class HeroDaoJdbcImpl.
      */
     @Test
     public void testUpdateHero() {
         System.out.println("updateHero");
-        Hero hero = null;
         HeroDaoJdbcImpl instance = new HeroDaoJdbcImpl();
+        Hero hero = new Hero();
+        hero.setHeroName("Wonder Woman");
+        hero.setDescription("classic superhero");
+        instance.addHero(hero);
+        hero.setDescription("has ability to fly");
         instance.updateHero(hero);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Hero result = instance.getHeroByID(hero.getHeroID());
+        assertEquals(hero, result);
     }
-
+    
     /**
      * Test of deleteHeroByID method, of class HeroDaoJdbcImpl.
      */
     @Test
     public void testDeleteHeroByID() {
         System.out.println("deleteHeroByID");
-        int heroID = 0;
         HeroDaoJdbcImpl instance = new HeroDaoJdbcImpl();
+        Hero hero = new Hero();
+        hero.setHeroName("Wonder Woman");
+        hero.setDescription("classic superhero");
+        instance.addHero(hero);
+        int heroID = hero.getHeroID();
+        Hero heroFromDao = instance.getHeroByID(heroID);
+        assertEquals(hero,heroFromDao);
         instance.deleteHeroByID(heroID);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of addHeroSuperPower method, of class HeroDaoJdbcImpl.
-     */
-    @Test
-    public void testAddHeroSuperPower() {
-        System.out.println("addHeroSuperPower");
-        int heroID = 0;
-        int superPowerID = 0;
-        HeroDaoJdbcImpl instance = new HeroDaoJdbcImpl();
-        instance.addHeroSuperPower(heroID, superPowerID);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of deleteHeroSuperPower method, of class HeroDaoJdbcImpl.
-     */
-    @Test
-    public void testDeleteHeroSuperPower() {
-        System.out.println("deleteHeroSuperPower");
-        int heroID = 0;
-        int superPowerID = 0;
-        HeroDaoJdbcImpl instance = new HeroDaoJdbcImpl();
-        instance.deleteHeroSuperPower(heroID, superPowerID);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of addHeroOrg method, of class HeroDaoJdbcImpl.
-     */
-    @Test
-    public void testAddHeroOrg() {
-        System.out.println("addHeroOrg");
-        int heroID = 0;
-        int orgID = 0;
-        HeroDaoJdbcImpl instance = new HeroDaoJdbcImpl();
-        instance.addHeroOrg(heroID, orgID);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of deleteHeroOrg method, of class HeroDaoJdbcImpl.
-     */
-    @Test
-    public void testDeleteHeroOrg() {
-        System.out.println("deleteHeroOrg");
-        int heroID = 0;
-        int orgID = 0;
-        HeroDaoJdbcImpl instance = new HeroDaoJdbcImpl();
-        instance.deleteHeroOrg(heroID, orgID);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertNull(instance.getHeroByID(heroID));
     }
     
 }
